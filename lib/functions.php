e6b3d414ea92c37fca5bf31a79f71561eeca70d0
vadym <vadym@xxx.(none)>
2013-01-11 16:36:07 +0200

    if function exist

--- lib/functions.php
+++ lib/functions.php
@@ -6,8 +6,9 @@
  * Time: 3:47 PM
  * To change this template use File | Settings | File Templates.
  */
-
-function __($string) {
-    $x_ls = $_SESSION['x_ls'];
-    return $x_ls->__($string);
+if(!function_exists('__')){
+    function __($string) {
+        $x_ls = $_SESSION['x_ls'];
+        return $x_ls->__($string);
+    }
 }
