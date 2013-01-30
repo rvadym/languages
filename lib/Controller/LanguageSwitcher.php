cf430087b9306ea8ea263d1a60f9ae1ac6b3ea47
vadym <vadym@xxx.(none)>
2013-01-30 22:58:14 +0200

    refactoring

--- lib/Controller/LanguageSwitcher.php
+++ lib/Controller/LanguageSwitcher.php
@@ -6,8 +6,26 @@
  * Time: 9:28 AM
  * To change this template use File | Settings | File Templates.
  */
+
+/*
+
+
+//                $before = memory_get_usage();
+
+                    create object here
+
+//                $after = memory_get_usage();
+//                var_dump(($after - $before)/(1024*1024));
+
+
+
+ */
+
+
+
 namespace x_ls;
 require_once __DIR__.'/../functions.php';
+require_once __DIR__."/../../spyc/spyc.php";
 
 class Controller_LanguageSwitcher extends \Controller {
     public $file_extension = 'yml';
@@ -19,64 +37,99 @@ class Controller_LanguageSwitcher extends \Controller {
     function init() {
         parent::init();
         $_SESSION['x_ls'] = $this;
-        $this->check();
         $this->api->x_ls = $this;
+        if (!$this->translation_dir_path) $this->translation_dir_path = $this->api->pm->base_directory.'translations';
 
-        $this->l = $this->checkLanguage();
-        $this->redir_url=$this->getUrl();
+        $this->switchLanguageIfRequired();
+        $this->getLanguage();
         $this->addLangSwitcher();
     }
+
+    ////////  translation  ////////
+    private $translations = array();
     function __($string){
-        if (!$this->translated) return $string;
-        if (array_key_exists($string,$this->translated)) {
-            return $this->translated[$string];
+        if (count($this->translations)==0) $this->translate();
+        if ($this->model) {
+            if (array_key_exists($string,$this->translations)) {
+                return $this->translations[$string];
+            }
+        } else {
+            if (array_key_exists($string,$this->translations)) {
+                return $this->translations[$string];
+            }
         }
         return '-'.$string.'-';
     }
-    private $translated = false;
-    function translate() {
-        $files = scandir($this->translation_dir_path);
-        foreach ($files as $file) {
-            if ($file != $this->l.'.'.$this->file_extension) continue;
-            $this->translated = \Spyc::YAMLLoad($this->translation_dir_path.'/'.$file);
+    private function translate() {
+        if($this->model) {
+            $t_trans = $this->model->getRows();
+            foreach ($t_trans as $t) {
+                if (!array_key_exists($this->l,$t)) continue;
+                $this->translations[$t['value']] = $t[$this->l];
+            }
+        } else {
+            $files = scandir($this->translation_dir_path);
+            foreach ($files as $file) {
+                if ($file != $this->l.'.'.$this->file_extension) continue;
+                $this->translations = \Spyc::YAMLLoad($this->translation_dir_path.'/'.$file);
+            }
         }
         return $this;
     }
+
+    // do not use directly, use $this->getLanguage() instead.
+    private $l = false;
+    function getLanguage(){
+        if (count($this->languages)==0) throw $this->exception('Provide language set.');
+        if ($this->l) return $this->l;
+        if ($this->recallLang()) return $this->l = $this->recallLang();
+        if ($this->default_language) {
+            $this->memorizeLang($this->default_language);
+            return $this->l = $this->default_language;
+        } else {
+            $this->memorizeLang($this->languages[0]);
+            return $this->l = $this->languages[0];
+        }
+    }
+    private function memorizeLang($lang) {
+        $this->memorize('user_panel_lang',$lang);
+    }
+    private function recallLang() {
+        return $this->recall('user_panel_lang');
+    }
+    private function switchLanguageIfRequired() {
+        if ($_GET['user_panel_lang']) {
+            $this->memorizeLang($_GET['user_panel_lang']);
+            $this->api->redirect();
+        }
+    }
+    private function addLangSwitcher() {
+        $v = $this->api->add('View',null,'lang_switcher');
+        foreach ($this->languages as $lang) {
+            $lv = $v->add('View')->addStyle('float','right');
+            if ($lang == $this->getLanguage()) {
+                $lv->setHTML('&nbsp;'.$lang.'&nbsp;');
+            } else {
+                $lv->setHTML('&nbsp;<a href="'.$this->getRedirUrl().$lang.'">'.$lang.'</a>&nbsp;');
+            }
+        }
+    }
+    private function getRedirUrl() {
+        $url = $_SERVER["REQUEST_URI"];
+        $url .= ((substr_count($url,'?')?'&user_panel_lang=':'?user_panel_lang='));
+        return $url;
+    }
+
+    ///////////     addon config      //////////////
     function defaultTemplate() {
 		// add add-on locations to pathfinder
 		$this->l = $this->api->locate('addons',__NAMESPACE__,'location');
 		$addon_location = $this->api->locate('addons',__NAMESPACE__);
 		$this->api->pathfinder->addLocation($addon_location,array(
-//			'js'=>'templates/js',
-//			'css'=>'templates/css',
-//            'template'=>'templates',
+			//'js'=>'templates/js',
+			//'css'=>'templates/css',
+            //'template'=>'templates',
 		))->setParent($this->l);
         parent::defaultTemplate();
-    }
-    private function check() {
-        if (count($this->languages)==0) throw $this->exception('Provide language set.');
-        if (!$this->default_language) $this->default_language = $this->languages[0];
-        if (!$this->translation_dir_path) $this->translation_dir_path = $this->api->pm->base_directory.'translations';
-        require_once __DIR__."/../../spyc/spyc.php";
-    }
-    private function addLangSwitcher() {
-        $v = $this->api->add('View',null,'lang_switcher');
-        foreach ($this->languages as $lang) {
-            $v->add('View')->addStyle('float','right')->setHTML('&nbsp;<a href="'.$this->redir_url.$lang.'">'.$lang.'</a>&nbsp;');
-        }
-    }
-    private function checkLanguage() {
-        if ($session_lang = $_GET['user_panel_lang']) {
-            $this->memorize('user_panel_lang',$_GET['user_panel_lang']);
-            $this->api->redirect();
-        }
-        if ($session_lang = $this->recall('user_panel_lang')) return $session_lang;
-        return $this->default_language;
-    }
-    private function getUrl() {
-        $url = $_SERVER["REQUEST_URI"];
-        str_replace('#','',$url);
-        $url .= ((substr_count($url,'?')?'&user_panel_lang=':'?user_panel_lang='));
-        return $url;
     }
 }
