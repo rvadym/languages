152e630338c8dbb841476dae391effe59ca58a03
vadym <vadym@xxx.(none)>
2013-11-25 19:28:30 +0200

    Init

---
+++ Init.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: vadym
+ * Date: 11/25/13
+ * Time: 7:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Init extends \AbstractController {
+    function init() {
+        parent::init();
+
+    }
+}
