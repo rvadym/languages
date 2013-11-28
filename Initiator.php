1b8a8bea5facdc9efa3af73c256a72d66683ec77
vadym <vadym@xxx.(none)>
2013-11-28 18:18:40 +0200

    up

--- Initiator.php
+++ Initiator.php
@@ -6,10 +6,13 @@
  * Time: 7:17 PM
  * To change this template use File | Settings | File Templates.
  */
-class Init extends \AbstractController {
+namespace rvadym\languages;
+class Initiator extends \AbstractController {
+    public $configs = array();
     public $addon_obj;
     function init() {
         parent::init();
+        $this->configs = $this->api->getConfigs($this->addon_obj->name,array());
         $this->api->addHook('localizeString',array($this,'_()'));
 
     }
