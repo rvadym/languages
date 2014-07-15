bc16d13d5fda934fc94bc49f0c11a11eeb22d087
Vyze <voozel@gmail.com>
2014-07-15 16:51:23 +0300

    Added model Language.php. Fix for use it.

--- doc/languages.sql
+++ doc/languages.sql
@@ -1,10 +1,11 @@
 -- -----------------------------------------------------
--- Table `languages`.`language`
+-- Table `language`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `language` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   `ui_name` VARCHAR(45) NULL,
+  `is_active` TINYINT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`))
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8
@@ -12,12 +13,12 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `languages`.`translation`
+-- Table  `translation`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `translation` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `value` VARCHAR(128) NOT NULL DEFAULT '',
-  `en` VARCHAR(45) NULL,
+  `en` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8
--- lib/Model/Language.php
+++ lib/Model/Language.php
@@ -3,6 +3,7 @@
  * Created by Vladyslav Polyukhovych
  * Date: 28.06.14
  */
+namespace rvadym\languages;
 class Model_Language extends \SQL_Model{
 
     public $table ='language';
@@ -10,6 +11,48 @@ class Model_Language extends \SQL_Model{
     function init(){
         parent::init();
 
+        //fields
+        $this->addField('name');
+        $this->addField('ui_name');
+        $this->addField('is_active')->type('bool');
+    }
+
+    /**
+     * @param array - List of languages. Use hashes to set parameters for every record.
+     * example:
+     $languages=('en','ru','ua'); //simple array
+     $languages= array( // array of hashes
+        'en'=>array('name'=>'en','ui_name'=>'English','is_active'=>1)
+        'lv'=>array('name'=>'lv','ui_name'=>'Latvian','is_active'=>0)
+     );
+     */
+    public function setDefaultData($languages = null){
+
+        if(!is_array($languages)) return;
+
+        $records = array();
+
+        //form records
+        foreach ($languages as $lang) {
+            if(is_array($lang)){
+
+                //check for required fields
+                if(!$lang['name']){
+                    $lang['name']=key($languages);
+                }
+
+                $records[]=$lang;
+
+            }else{  //simple array
+                $records[]= array('name'=>$lang, 'ui_name'=>$lang, 'is_active'=>1);
+            }
+        }
+
+        //insert records
+        if($records){
+            $q = $this->dsql();
+            $q->insertAll($records);
+        }
     }
 
 }
